ad5e9984625dd657bd2c6a5d093e1d818111c271
Mikael Henaff <mikael314@gmail.com>
2018-03-19 22:09:15 -0400

    update scripts

--- scripts/run_train_fm.sh
+++ scripts/run_train_fm.sh
@@ -3,15 +3,17 @@
 rm *.err
 rm *.out
 
-for model in fwd-cnn-vae-lp; do 
-    for lrt in 0.0001; do 
-        for nfeature in 128; do 
-            for nhidden in 100; do 
-                for ncond in 10; do 
-                    for npred in 20; do 
-                        for nz in 32; do 
-                            for beta in 0.0002 0.0003; do 
-                                sbatch submit_train_fm.slurm $model $lrt $nfeature $nhidden $ncond $npred $beta $nz
+for dataset in i80; do
+    for model in fwd-cnn; do 
+        for lrt in 0.0001; do 
+            for nfeature in 96; do 
+                for nhidden in 100; do 
+                    for ncond in 10; do 
+                        for npred in 30 40 50; do 
+                            for nz in 1; do 
+                                for beta in 1.0; do 
+                                    sbatch submit_train_fm.slurm $model $lrt $nfeature $nhidden $ncond $npred $beta $nz $dataset
+                                done
                             done
                         done
                     done
