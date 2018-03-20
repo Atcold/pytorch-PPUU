5fac66c85ca580ba98a2f324d1f04bf87e4bd7ce
mikaelhenaff <mikael314@gmail.com>
2018-03-20 04:00:32 -0400

    update test

--- test_fm.py
+++ test_fm.py
@@ -10,6 +10,7 @@ from gym.envs.registration import register
 import scipy.misc
 
 parser = argparse.ArgumentParser()
+parser.add_argument('-v', type=int, default=1)
 parser.add_argument('-display', type=int, default=1)
 parser.add_argument('-seed', type=int, default=9999)
 parser.add_argument('-lanes', type=int, default=8)
@@ -26,19 +27,36 @@ random.seed(opt.seed)
 np.random.seed(opt.seed)
 torch.manual_seed(opt.seed)
 
+tags = {'wrapper_config.TimeLimit.max_episodesteps': 100}
+kwargs = {
+    'display': opt.display,
+    'nb_lanes': opt.lanes,
+    'traffic_rate': opt.traffic_rate,
+    'state_image': 1,
+    'store': 1,
+}
+
 register(
     id='Traffic-v0',
     entry_point='traffic_gym:StatefulEnv',
-    tags={'wrapper_config.TimeLimit.max_episodesteps': 100},
-    kwargs={'display': opt.display,
-            'nb_lanes': opt.lanes,
-            'traffic_rate': opt.traffic_rate},
+    tags=tags,
+    kwargs=kwargs
 )
 
-env = gym.make('Traffic-v0')
+register(
+    id='Traffic-v1',
+    entry_point='traffic_gym_v1:RealTraffic',
+    tags=tags,
+    kwargs=kwargs
+)
+
+env = gym.make('Traffic-v' + str(opt.v))
+
+
+mdir = 'dataset_i80/'
 #opt.mfile = 'model=fwd-cnn-een-fp-bsize=32-ncond=10-npred=20-lrt=0.0001-nhidden=100-nfeature=128-sigmout=1-tieact=0-nz=8.model'
-opt.mfile = 'model=fwd-cnn-vae-lp-bsize=32-ncond=10-npred=20-lrt=0.0001-nhidden=100-nfeature=96-tieact=0-zeroact-nz=32-beta=0.01-warmstart=1.model'
-model = torch.load('models_20-shards/' + opt.mfile)
+opt.mfile = 'model=fwd-cnn-bsize=32-ncond=10-npred=30-lrt=0.0001-nhidden=100-nfeature=96-tieact=0-warmstart=0.model'
+model = torch.load(mdir + '/models/' + opt.mfile)
 model.opt.tie_action = 0
 model.opt.npred = opt.npred
 
@@ -49,14 +67,16 @@ def run_episode():
 
     state, objects = env.reset()
     for t in range(2000):
-        if t > 300:
+        if t > 500:
             vid = 0
+            vehicles = env.vehicles
             for v in vehicles:
                 print('here')
                 images = torch.stack(v._states_image).permute(0, 3, 2, 1).float()
                 images /= 255.0
                 images = Variable(images[-opt.ncond:].clone().float().unsqueeze(0))
 
+                dirname = f'{mdir}/videos/{opt.mfile}'
                 actions = torch.zeros(1, opt.npred, 3)
                 actions[:, :, 2].fill_(0)
                 actions[:, :, 1].fill_(+0.0)
@@ -64,76 +84,14 @@ def run_episode():
                     pred, _ = model(images, Variable(actions), None)
                     print(pred.norm())
                     pred = pred.squeeze().permute(0, 2, 3, 1).data.numpy()
-                    dirname = 'videos/{}/pred{:d}/action0/sample{:d}/'.format(opt.mfile, vid, s)
-                    os.system("mkdir -p " + dirname)
+                    dirname_movie = '{}/pred{:d}/action0/sample{:d}/'.format(dirname, vid, s)
+                    os.system("mkdir -p " + dirname_movie)
                     for t in range(opt.npred):
                         if t > 0:
                             p = (pred[t] + pred[t-1])/2
                         else:
                             p = pred[t]
-                        scipy.misc.imsave(dirname + '/im{:05d}.png'.format(t), p)
-
-
-                actions = torch.zeros(1, opt.npred, 3)
-                actions[:, :, 2].fill_(+1.0)
-                actions[:, :, 1].fill_(+0.0)
-                for s in range(opt.n_samples):
-                    pred, _ = model(images, Variable(actions), None)
-                    pred = pred.squeeze().permute(0, 2, 3, 1).data.numpy()
-                    dirname = 'videos/{}/pred{:d}/action1/sample{:d}/'.format(opt.mfile, vid, s)
-                    os.system("mkdir -p " + dirname)
-                    for t in range(opt.npred):
-                        if t > 0:
-                            p = (pred[t] + pred[t-1])/2
-                        else:
-                            p = pred[t]
-                        scipy.misc.imsave(dirname + '/im{:05d}.png'.format(t), p)
-
-                actions = torch.zeros(1, opt.npred, 3)
-                actions[:, :, 2].fill_(-1.0)
-                actions[:, :, 1].fill_(+0.0)
-                for s in range(opt.n_samples):
-                    pred, _ = model(images, Variable(actions), None)
-                    pred = pred.squeeze().permute(0, 2, 3, 1).data.numpy()
-                    dirname = 'videos/{}/pred{:d}/action2/sample{:d}/'.format(opt.mfile, vid, s)
-                    os.system("mkdir -p " + dirname)
-                    for t in range(opt.npred):
-                        if t > 0:
-                            p = (pred[t] + pred[t-1])/2
-                        else:
-                            p = pred[t]
-                        scipy.misc.imsave(dirname + '/im{:05d}.png'.format(t), p)
-
-                actions = torch.zeros(1, opt.npred, 3)
-                actions[:, :, 2].fill_(0.0)
-                actions[:, :, 1].fill_(+20.0)
-                for s in range(opt.n_samples):
-                    pred, _ = model(images, Variable(actions), None)
-                    pred = pred.squeeze().permute(0, 2, 3, 1).data.numpy()
-                    dirname = 'videos/{}/pred{:d}/action3/sample{:d}/'.format(opt.mfile, vid, s)
-                    os.system("mkdir -p " + dirname)
-                    for t in range(opt.npred):
-                        if t > 0:
-                            p = (pred[t] + pred[t-1])/2
-                        else:
-                            p = pred[t]
-                        scipy.misc.imsave(dirname + '/im{:05d}.png'.format(t), p)
-
-                actions = torch.zeros(1, opt.npred, 3)
-                actions[:, :, 2].fill_(0.0)
-                actions[:, :, 1].fill_(-20.0)
-                for s in range(opt.n_samples):
-                    pred, _ = model(images, Variable(actions), None)
-                    pred = pred.squeeze().permute(0, 2, 3, 1).data.numpy()
-                    dirname = 'videos/{}/pred{:d}/action4/sample{:d}/'.format(opt.mfile, vid, s)
-                    os.system("mkdir -p " + dirname)
-                    for t in range(opt.npred):
-                        if t > 0:
-                            p = (pred[t] + pred[t-1])/2
-                        else:
-                            p = pred[t]
-                        scipy.misc.imsave(dirname + '/im{:05d}.png'.format(t), p)
-
+                        scipy.misc.imsave(dirname_movie + '/im{:05d}.png'.format(t), p)
                 vid += 1
 
         else:
