12f55df306ca56e5e8faebe7f5414b16695ae616
Mikael Henaff <mikael314@gmail.com>
2018-03-28 19:47:55 -0400

    add PCA and tSNE embeddings

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import torch
-import os, json, pdb
+import os, json, pdb, math, numpy
 from datetime import datetime
+from sklearn import decomposition
+from sklearn.manifold import TSNE
 
 # Logging function
 def log(fname, s):
@@ -10,9 +12,38 @@ def log(fname, s):
     f.write(str(datetime.now()) + ': ' + s + '\n')
     f.close()
 
+def grad_norm(net):
+    total_norm = 0
+    for p in net.parameters():
+        param_norm = p.grad.data.norm(2)
+        total_norm += param_norm ** 2
+    total_norm = total_norm ** (1. / 2)
+    return total_norm
 
 def read_config(file_path):
     """Read JSON config."""
     json_object = json.load(open(file_path, 'r'))
     return json_object
 
+def log_pdf(z, mu, sigma):
+    a = 0.5*torch.sum(((z-mu)/sigma)**2, 1)
+    b = torch.log(2*math.pi*torch.prod(sigma, 1))
+    loss = a.squeeze() + b.squeeze()
+    return torch.mean(loss)
+
+# embed Z distribution as well as some special z's (ztop) using PCA and tSNE. 
+# Useful for visualizing predicted z vectors. 
+def embed(Z, ztop):
+    bsize = ztop.shape[0]
+    nsamples = ztop.shape[1]
+    dim = ztop.shape[2]
+    ztop = ztop.reshape(bsize*nsamples, dim)
+    pca = decomposition.PCA(n_components=2)
+    pca.fit(Z)
+    Z_pca = pca.transform(Z)
+    ztop_pca = pca.transform(ztop).reshape(bsize, nsamples, 2)
+    Z_all=numpy.concatenate((ztop, Z), axis=0)
+    Z_all_tsne = TSNE(n_components=2).fit_transform(Z_all)
+    ztop_tsne = Z_all_tsne[0:bsize*nsamples].reshape(bsize, nsamples, 2)
+    Z_tsne = Z_all_tsne[bsize*nsamples:]
+    return {'Z_pca': Z_pca, 'ztop_pca': ztop_pca, 'Z_tsne': Z_tsne, 'ztop_tsne': ztop_tsne}
