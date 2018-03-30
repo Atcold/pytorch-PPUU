5bb2d6d3f590a85b739a442773c76739f0ec4e87
Mikael Henaff <mikael314@gmail.com>
2018-03-30 14:55:18 -0400

    update

--- scripts/run_train_fm.sh
+++ scripts/run_train_fm.sh
@@ -4,15 +4,15 @@ rm *.err
 rm *.out
 
 for dataset in i80; do
-    for model in fwd-cnn; do 
+    for model in fwd-cnn-ae-fp; do 
         for lrt in 0.0001; do 
             for nfeature in 96; do 
-                for nhidden in 100; do 
+                for warmstart in 1; do 
                     for ncond in 10; do 
-                        for npred in 30 40 50; do 
-                            for nz in 1; do 
+                        for npred in 20; do 
+                            for nz in 128 256; do 
                                 for beta in 1.0; do 
-                                    sbatch submit_train_fm.slurm $model $lrt $nfeature $nhidden $ncond $npred $beta $nz $dataset
+                                    sbatch submit_train_fm.slurm $model $lrt $nfeature $warmstart $ncond $npred $beta $nz $dataset
                                 done
                             done
                         done
--- scripts/run_train_il.sh
+++ scripts/run_train_il.sh
@@ -3,12 +3,12 @@
 rm *.err
 rm *.out
 
-for model in policy-cnn-een; do 
+for model in policy-ae; do 
     for lrt in 0.0001; do 
         for nhidden in 100; do 
             for ncond in 10; do 
                 for npred in 10 20; do 
-                    for nz in 1 2 4 8 16; do 
+                    for nz in 1 2 4 8; do 
                         for beta in 1; do 
                             for nshards in 20; do 
                                 sbatch submit_train_il.slurm $model $lrt $nhidden $ncond $npred $beta $nshards $nz
