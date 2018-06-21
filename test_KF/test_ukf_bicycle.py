612c534cd2ab1d1a418596b86433866031e3c98d
Bo Liu <bo.robotics@gmail.com>
2018-06-20 22:58:18 -0400

    closing branch

--- test_KF/test_ukf_bicycle.py
+++ test_KF/test_ukf_bicycle.py
@@ -13,13 +13,18 @@ def plot_phi(x, y, phi, length):
 
     plt.plot([x, endx], [y, endy], 'g-')
 
+
 class trj(object):
+    """
+    this is the data type stored in pickle
+    """
 
     def __init__(self, xdata=[], ydata=[]):
         self.x = xdata
         self.y = ydata
 
 
+# from data_i80/trajectories-0400-0415
 car_list = [1,100,200,400,250]
 car_data = []
 for i, car in enumerate(car_list):
@@ -28,27 +33,30 @@ for i, car in enumerate(car_list):
 
 
 ukf_car = []
-for car in car_list:
-    ukf_car.append(ukf.ukf())
-
+for i, car in enumerate(car_list):
+    px, py = car_data[i].x[0], car_data[i].y[0]
+    ukf_car.append(ukf.Ukf(dt=0.1, startx=px, starty=py))
 
 px_kf_list = []
 py_kf_list = []
+speed_kf_list = []
 phi_kf_list = []
 for i, car in enumerate(car_list):
-    print(i)
+    print("processing car # {}".format(car))
     px_kf = []
     py_kf = []
+    speed_kf = []
     phi_kf = []
     for px, py in zip(car_data[i].x, car_data[i].y):
-        ukf_car[i].predict()
         z = np.array([px, py])
-        ukf_car[i].update(z)
+        ukf_car[i].step(z)
         px_kf.append(ukf_car[i].ukf.x[0])
         py_kf.append(ukf_car[i].ukf.x[1])
+        speed_kf.append(ukf_car[i].ukf.x[2])
         phi_kf.append(ukf_car[i].ukf.x[3])
     px_kf_list.append(px_kf)
     py_kf_list.append(py_kf)
+    speed_kf_list.append(speed_kf)
     phi_kf_list.append(phi_kf)
 
 fig = plt.figure(figsize=(20, 4))
@@ -56,10 +64,10 @@ plt.subplot(len(car_list),1,1)
 
 for i, car in enumerate(car_list):
     ax = plt.subplot(len(car_list), 1, i+1)
-    plt.scatter(car_data[i].x, car_data[i].y, s=1)
+    plt.scatter(car_data[i].x, car_data[i].y, s=10,)
     plt.plot(px_kf_list[i], py_kf_list[i], 'r-')
-    for px,py, phi in zip(px_kf_list[i], py_kf_list[i], phi_kf_list[i]):
-        plot_phi(px, py, phi, 1)
+    for px,py,speed,phi in zip(px_kf_list[i], py_kf_list[i], speed_kf_list[i], phi_kf_list[i]):
+        plot_phi(px, py, phi, speed*0.5)
     plt.ylabel('car ' + str(car))
     ax.yaxis.set_label_position("right")
 
--- ukfBicycle/ukf.py
+++ ukfBicycle/ukf.py
@@ -6,55 +6,73 @@ from math import tan, sin, cos, sqrt, atan2
 
 @jit
 def move(x, dt, wheelbase=2.5, u=np.array([0.,0.]), eps=1e-5):
-    # state vector: (px, py, v, phi)
+    """
+    Bicyle motion model for a Ukf.
+    :param x: state vector [px (m), py (m), speed (m/s), hdg (rad)]
+    :param dt: dt
+    :param wheelbase: axle distance of a two-axle vehicle
+    :param u: control command if issued: [acceleration (m/s**2), steering angle (rad)]
+    :param eps: is used for small number test
+    :return: predicted state vector
+    """
+    # state vector: (px, py, speed, phi)
     hdg = x[3]
-    vel = x[2]
+    speed = x[2]
     steering_angle = u[1]
     acc = u[0]
-    dist = vel * dt + 0.5 * acc * dt**2
+    distance_traveled = speed * dt + 0.5 * acc * dt**2
 
-    if abs(steering_angle) > eps:  # is robot turning?
-        beta = (dist / wheelbase) * tan(steering_angle)
-        r = wheelbase / tan(steering_angle)  # radius
+    if abs(steering_angle) > eps:  # non-zero steering: turning
+        # beta = change of heading (rad)
+        beta = (distance_traveled / wheelbase) * tan(steering_angle)
+        # r = turn radius w.r.t. rear axle
+        r = wheelbase / tan(steering_angle)
 
         sinh, sinhb = sin(hdg), sin(hdg + beta)
         cosh, coshb = cos(hdg), cos(hdg + beta)
 
         return x + np.array([-r * sinh + r * sinhb, r * cosh - r * coshb, acc*dt, beta])
-    else:  # moving in straight line
-        return x + np.array([dist * cos(hdg), dist * sin(hdg), acc * dt, 0])
+    else:  # moving in a straight line
+        return x + np.array([distance_traveled * cos(hdg), distance_traveled * sin(hdg), acc * dt, 0])
 
 @jit
-def normalize_angle(x):
-    x = x % (2 * np.pi)    # force in range [0, 2 pi)
-    if x > 0.5*np.pi:          # move to [-pi, pi)
-        x -= 2 * np.pi
-    return x
+def normalize_angle(hdg):
+    """
+    normalize hdg radian number to [-PI, PI]
+    :param hdg: an angle
+    :return: normalized angle
+    """
+    hdg = hdg % (2 * np.pi)
+    if hdg > 0.5*np.pi:
+        hdg -= 2 * np.pi
+    return hdg
 
-
-# @jit
-# def residual_h(a, b):
-#     # measurement is (position_x, position_y)
-#     z = a - b
-#     return z
 
 @jit
 def residual_x(a, b):
     x = a - b
-    # state vector is (x, y, v, phi)
+    # state vector is (x, y, speed, hdg)
     x[3] = normalize_angle(x[3])
     return x
 
 @jit
 def Hx(x):
-    """ takes a state variable and returns the measurement
-    that would correspond to that state. """
-
+    """
+    measurement function: maps to measurement space: (px, py)
+    :param x: state vector
+    :return: state vector in measurement space
+    """
     return np.array([x[0], x[1]])
 
 
 @jit
 def state_mean(sigmas, Wm):
+    """
+    functor for the Ukf to estimate mean of state vector
+    :param sigmas: sigma points
+    :param Wm: weights
+    :return: state mean estimate
+    """
     x = np.zeros(4)
 
     x[0] = np.sum(np.dot(sigmas[:, 0], Wm))
@@ -67,25 +85,78 @@ def state_mean(sigmas, Wm):
     return x
 
 
-class ukf(object):
+class Ukf(object):
+    """
+    Unscented Kalman Filter with augmented bicycle motion model.
+    state vector: [position_x (m), position_y (m), vehicle_speed (m/s), vehicle_heading (rad)]
+    control cmd: [acceleration (m/s**2), steering angle (rad)]
+    """
 
-    def __init__(self):
-        # state vector: [px, py, phi]
-        self.dt = 0.1
+    def __init__(self, dt=0.1, wheelbase=2.5, startx=0., starty=0., startspeed=0., starthdg=0., stdx=1.0, stdy=1.0, noise=0.1):
+        """
+        initialize Ukf
+        :param dt: dt
+        :param wheelbase: axle distance of a two-axle vehicle (default = 2.5m)
+        :param startx: initial position x (default = 0m)
+        :param starty: initial position y (default = 0m)
+        :param startspeed: initial speed (default = 0m/s)
+        :param starthdg: initial heading (default = 0 rad, x-axis aligned)
+        :param stdx: std of measurement for position x (default = 1.0m)
+        :param stdy: std of measurement for position y (default = 1.0m)
+        :param noise: process noise for motion model (default = 0.1m)
+        """
+        # state vector: [px, py, speed, hdg (rad)]
+        self.dt = dt
+        self.x = np.array([startx, starty, startspeed, starthdg])
+        self.wheelbase = wheelbase
         self.points = MerweScaledSigmaPoints(n=4, alpha=.00001, beta=2, kappa=1, subtract=residual_x)
         self.ukf = UKF.UnscentedKalmanFilter(dim_x=4, dim_z=2, dt=self.dt, fx=move, hx=Hx, points=self.points,
                                              x_mean_fn=state_mean, residual_x=residual_x)
-        self.ukf.x = np.array([0.,0.,0.,0.])
+        self.ukf.x = np.array([startx, starty, startspeed, starthdg])
+        # initial guess
         self.ukf.P = np.array([[1, 0, 1, 0],
                                [0, 1, 1, 0],
                                [1, 1, 2, 0],
                                [0, 0, 0, 1]])
 
-        self.ukf.R = np.diag([0.5**2, 0.25**2])
-        self.ukf.Q = np.eye(4)*0.001
+        # estimated from data
+        self.ukf.P = np.array([[ 4.78559217e-01, -1.94838441e-03,  2.48955909e-01, -1.25313018e-03],
+                               [-1.94838441e-03,  7.63804713e-01,  1.69689503e-03,  1.83459578e-01],
+                               [ 2.48955909e-01,  1.69689503e-03,  1.51635257e+00,  6.06738360e-06],
+                               [-1.25313018e-03,  1.83459578e-01,  6.06738360e-06,  2.18001626e-01]])
+
+        self.ukf.R = np.diag([stdx**2, stdy**2])
+        self.ukf.Q = np.eye(4)*noise
 
     def predict(self):
-        self.ukf.predict()
+        """
+        prediction step of Ukf using a motion model
+        :return:
+        """
+        self.ukf.predict(wheelbase=self.wheelbase)
 
     def update(self, meas):
+        """
+        update Ukf with a measurement
+        :param meas: measurement vector [px, py]
+        :return: void
+        """
         self.ukf.update(meas)
+
+    def step(self, meas):
+        """
+        client code interface to update Ukf with a new measurement
+        :param meas: measurement vector [px, py]
+        :return: void
+        """
+        self.predict()
+        self.update(meas)
+
+    def get_position(self):
+        return np.array([self.ukf.x[0], self.ukf.x[1]])
+
+    def get_heading(self):
+        return self.ukf.x[3]
+
+    def get_speed(self):
+        return self.ukf.x[2]
