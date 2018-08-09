8d08bb1f0fd0033e456ccde400bcf869a7cd2dde
Alfredo Canziani <alfredo.canziani@gmail.com>
2018-08-08 23:23:54 -0400

    Update and add slurm generation data

--- scripts/run_generate_data.sh
+++ scripts/run_generate_data.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-for i in 0 1 2; do 
-    sbatch submit_generate_data.slurm $i
+# Pick the map you'd like to generate data for (comment the others)
+MAP="ai"
+MAP="i80"
+MAP="us101"
+# MAP="lanker"
+
+if $(echo "i80 us101" | grep -q $MAP); then T=2; fi
+if $(echo "lanker" | grep -q $MAP); then T=1; fi
+
+echo "Map: $MAP, time slots: $(eval echo {0..$T})"
+
+
+for t in $(eval echo {0..$T}); do  # time slot
+    sbatch \
+        --output ${MAP}_ts${t}.out \
+        --error ${MAP}_ts${t}.err \
+        submit_generate_data_${MAP}.slurm $t
 done
