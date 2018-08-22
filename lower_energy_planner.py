0c8e2a5aadabb8e3ba73bbe9ebafa8eeb8ade2cd
Etienne <etiennedejoie@gmail.com>
2018-08-22 00:05:59 -0400

    first policy, gradient descent to determine the position with lowest energy on a, b=0

--- lower_energy_planner.py
+++ lower_energy_planner.py
@@ -2,6 +2,7 @@ import argparse
 import numpy
 import gym
 import pdb
+import torch
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-seed', type=int, default=0)
@@ -37,27 +38,49 @@ def get_controlled_car(env):
     else :
         return None
 
-def distance(car1, car2):
-    diff = car1.get_state()[:2] - car2.get_state()[:2]
-    return torch.norm(diff)
+def dE_dxj(env, autonomous_car_id, x, y):
+    tot = 0
+    energy_vector = numpy.array([1, 10])
+
+    for car in env.vehicles:
+        if car.id is not autonomous_car.id:
+            cx, cy = car.get_state().numpy()[:2] + car.get_state().numpy()[2:]*car._dt
+            ener = -10000*energy_vector[0]*(x - cx)/(energy_vector[0]*(cx - x)**2 + energy_vector[1]*(cy - y)**2)**2
+            #if car.id == 700:
+                #print(ener)
+            tot += ener
+    return tot
 
 def get_total_energy(env, autonomous_car):
     energy = 0
     for car in env.vehicles:
         if car.id is not autonomous_car.id:
-            energy += 1./(distance(car, autonomous_car))
+            energy += compute_energy(car.get_state()[:2], autonomous_car.get_state()[:2])
+            print(energy)
     return energy
 
 for episode in range(1000):
     env.reset()
 
+    max_a = 30
     done = False
-    autonomous_car = env
+    a, b = 0., 0.
     while not done:
         autonomous_car = get_controlled_car(env)
         if autonomous_car:
-            print("total energy is %f" % get_total_energy(env, autonomous_car))
-        observation, reward, done, info = env.step(numpy.array((0,0)))
+            #print("total energy is %f" % get_total_energy(env, autonomous_car))
+            x_o, y_o, dx_o, dy_o = autonomous_car.get_state().numpy()
+            x, y = x_o, y_o
+            #print("starting x = {}".format(x))
+            #print(dx_o)
+            for i in range(20):
+                x -= 10*dE_dxj(env, autonomous_car.id, x, y)
+
+            #print("x = {}".format(x))
+            a = ((x - x_o)/ autonomous_car._dt -  dx_o) / autonomous_car._dt
+            a = a if abs(a) < max_a else numpy.sign(a) * max_a
+
+        observation, reward, done, info = env.step(numpy.array((a,0)))
 
         env.render()
 
