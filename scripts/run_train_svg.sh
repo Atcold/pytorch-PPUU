f93e18af75929fb9c9ef582dc8730bb3fd8b82de
Mikael Henaff <mikael314@gmail.com>
2018-08-26 17:39:58 -0400

    scripts

--- scripts/run_train_svg.sh
+++ scripts/run_train_svg.sh
@@ -2,11 +2,13 @@
 
 for npred in 20; do 
     for bsize in 12; do 
-        for u_reg in 0.0 0.1 0.5 1.0 2.0; do 
+        for u_reg in 0.1 0.2 0.5 1.0; do 
             for lambda_a in 0.0; do 
                 for z_updates in 0; do 
                     for lrt_z in 0.0; do 
-                        sbatch submit_train_svg.slurm $npred $u_reg $lrt_z $z_updates $bsize
+                        for lambda_l in 0.0 0.05 0.1; do 
+                            sbatch submit_train_svg.slurm $npred $u_reg $lrt_z $z_updates $bsize $lambda_l
+                        done
                     done
                 done
             done
