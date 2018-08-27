19ce42b7f62f061cfa94f89e95b3dbde6b7f81d3
Mikael Henaff <mikael314@gmail.com>
2018-08-27 16:59:34 -0400

    rm files

--- scripts/run_plan_policy_tm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-model_dir=/home/mbhenaff/projects/pytorch-Traffic-Simulator/scratch/models_v9/
-
-for policy_model in ${model_dir}/policy_networks/mbil*.model; do 
-    echo $(basename $policy_model)
-    sbatch submit_plan_policy_tm.slurm $model_dir $(basename $policy_model)
-done
