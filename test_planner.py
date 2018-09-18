e9b0841ba7efd6ca3ec78c0ce53fab0af9201124
mikaelhenaff <mikael314@gmail.com>
2018-09-18 16:55:08 -0400

    iclr

--- test_planner.py
+++ test_planner.py
@@ -1,58 +1,48 @@
 import argparse
-import pdb
+import random
+import torch
 import numpy
 import gym
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-seed', type=int, default=0)
 parser.add_argument('-nb_conditions', type=int, default=10)
+parser.add_argument('-nb_predictions', type=int, default=10)
 parser.add_argument('-nb_samples', type=int, default=1)
-parser.add_argument('-display', type=int, default=1)
-parser.add_argument('-map', type=str, default='i80', choices={'i80', 'us101', 'lanker', 'peach'})
-parser.add_argument('-fps', type=int, default=1e3)
-parser.add_argument('-delta_t', type=float, default=0.1)
-parser.add_argument('-nb_episodes', type=int, default=10)
+parser.add_argument('-models_dir', type=str, default='./models_il/')
+parser.add_argument('-v', type=str, default='3', choices={'3'})
 
 opt = parser.parse_args()
 
+random.seed(opt.seed)
+numpy.random.seed(opt.seed)
+torch.manual_seed(opt.seed)
+
 kwargs = {
-    'fps': opt.fps,
+    'fps': 50,
     'nb_states': opt.nb_conditions,
-    'display': opt.display,
-    'delta_t': opt.delta_t,
 }
 
 gym.envs.registration.register(
-    id='I-80-v1',
-    entry_point='map_i80_ctrl:ControlledI80',
+    id='Traffic-v3',
+    entry_point='traffic_gym_v3:ControlledI80',
     kwargs=kwargs,
 )
 
-env_names = {
-    'i80': 'I-80-v1',
-}
-
 print('Building the environment (loading data, if any)')
-env = gym.make(env_names[opt.map])
+env = gym.make('Traffic-v' + opt.v)
 
-for episode in range(opt.nb_episodes):
-    context, states = list(), list()
+for episode in range(10):
 
-    observation = env.reset(time_slot=None, vehicle_id=None)  # if None => picked at random
-    context.append(observation['context'])
-    states.append(observation['state'])
+    env.reset()
 
     done = False
     while not done:
         observation, reward, done, info = env.step(numpy.zeros((2,)))
-
-        context.append(observation['context'])
-        states.append(observation['state'])
-        # print(observation, reward, done, info)
-
+        if observation is not None:
+            print(reward[0][-1], reward[1][-1])
+#        print(observation, reward, done, info)
         env.render()
-
-    print('Episode completed!')
-    pdb.set_trace()
+    print('end of episode')
 
 print('Done')
