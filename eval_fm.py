33773588e4fb8fc27431bf3d07da8978aacf8f88
Alfredo Canziani <alfredo.canziani@gmail.com>
2019-02-14 14:02:38 -0500

    Heavily clean up eval_fm.py

--- eval_fm.py
+++ eval_fm.py
@@ -1,14 +1,13 @@
-import argparse, pdb, os, pickle, random, sys
-import gym
+import argparse
+import os
+import random
+
 import numpy as np
 import torch
-import torch.nn.functional as F
-from torch.autograd import Variable
-import torch.nn as nn
-from gym.envs.registration import register
-import scipy.misc
-from dataloader import DataLoader
+import torch.nn.functional as fun
+
 import utils
+from dataloader import DataLoader
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-dataset', type=str, default='i80')
@@ -30,9 +29,15 @@ parser.add_argument('-noise', type=float, default=0.0)
 parser.add_argument('-n_mixture', type=int, default=20)
 parser.add_argument('-graph_density', type=float, default=0.001)
 parser.add_argument('-model_dir', type=str, default='/misc/vlgscratch4/LecunGroup/nvidia-collab/models_v11/')
-parser.add_argument('-mfile', type=str, default='model=fwd-cnn-vae-fp-layers=3-bsize=64-ncond=20-npred=20-lrt=0.0001-nfeature=256-dropout=0.1-nz=32-beta=1e-06-zdropout=0.5-gclip=5.0-warmstart=1-seed=1.step200000.model')
-#parser.add_argument('-mfile', type=str, default='model=fwd-cnn-ten3-layers=3-bsize=64-ncond=20-npred=20-lrt=0.0001-nfeature=256-nhidden=128-fgeom=1-zeroact=0-zmult=0-dropout=0.1-nz=32-beta=0.0-zdropout=0.5-gclip=5.0-warmstart=1-seed=1.step200000.model')
-#parser.add_argument('-mfile', type=str, default='model=fwd-cnn-layers=3-bsize=64-ncond=20-npred=20-lrt=0.0001-nfeature=256-dropout=0.1-gclip=5.0-warmstart=0-seed=1.step200000.model')
+M1 = 'model=fwd-cnn-layers=3-bsize=64-ncond=20-npred=20-lrt=0.0001-nfeature=256-dropout=0.1-gclip=5.0-' + \
+     'warmstart=0-seed=1.step200000.model'
+M2 = 'model=fwd-cnn-vae-fp-layers=3-bsize=64-ncond=20-npred=20-lrt=0.0001-nfeature=256-dropout=0.1-nz=32-' + \
+     'beta=1e-06-zdropout=0.0-gclip=5.0-warmstart=1-seed=1.step200000.model'
+M3 = 'model=fwd-cnn-vae-fp-layers=3-bsize=64-ncond=20-npred=20-lrt=0.0001-nfeature=256-dropout=0.1-nz=32-' + \
+     'beta=1e-06-zdropout=0.5-gclip=5.0-warmstart=1-seed=1.step200000.model'
+M4 = 'model=fwd-cnn-ten3-layers=3-bsize=64-ncond=20-npred=20-lrt=0.0001-nfeature=256-nhidden=128-fgeom=1-' + \
+     'zeroact=0-zmult=0-dropout=0.1-nz=32-beta=0.0-zdropout=0.5-gclip=5.0-warmstart=1-seed=1.step200000.model'
+parser.add_argument('-mfile', type=str, default=M3)
 parser.add_argument('-cuda', type=int, default=1)
 parser.add_argument('-save_video', type=int, default=1)
 opt = parser.parse_args()
@@ -55,14 +60,13 @@ model = torch.load(opt.model_dir + opt.mfile)
 if type(model) is dict: model = model['model']
 model = model.cuda()
 model.eval()
-if opt.cuda == 1:
-    model.intype('gpu')
+# if opt.cuda == 1:
+    # model.intype('gpu')
 
 dataloader = DataLoader(None, opt, opt.dataset)
-model.opt.npred = opt.npred
+# model.opt.npred = opt.npred  # instruct the model about how many predictions we want it to produce
 model.opt.alpha = 0
-model.disable_unet=False
-
+model.disable_unet = False
 
 dirname = f'{opt.eval_dir}/{opt.mfile}-nbatches={opt.n_batches}-npred={opt.npred}-nsample={opt.n_samples}'
 if '-ten' in opt.mfile:
@@ -82,7 +86,7 @@ if '-ten' in opt.mfile:
         model.p_z = p_z
         model.knn_indx = graph.get('knn_indx')
         model.knn_dist = graph.get('knn_dist')
-        model.opt.topz_sample = int(model.p_z.size(0)*opt.graph_density)            
+        model.opt.topz_sample = int(model.p_z.size(0) * opt.graph_density)
     else:
         model.compute_pz(dataloader, opt, 250)
         torch.save(model.p_z, pzfile)
@@ -93,9 +97,8 @@ if '-ten' in opt.mfile:
 dirname += '.eval'
 os.system('mkdir -p ' + dirname)
 
-
-if opt.cuda == 1:
-    model.intype('gpu')
+# if opt.cuda == 1:
+#     model.intype('gpu')
 
 loss_i = torch.zeros(opt.n_batches, opt.batch_size, opt.n_samples, opt.npred)
 loss_s = torch.zeros(opt.n_batches, opt.batch_size, opt.n_samples, opt.npred)
@@ -109,87 +112,78 @@ pred_states = torch.zeros(opt.n_batches, opt.batch_size, opt.n_samples, opt.npre
 def compute_loss(targets, predictions, r=True):
     pred_images, pred_states, _ = predictions
     target_images, target_states, target_costs = targets
-    loss_i = F.mse_loss(pred_images, target_images, reduce=r)
-    loss_s = F.mse_loss(pred_states, target_states, reduce=r)
-    loss_c = F.mse_loss(pred_costs.cuda(), target_costs.cuda(), reduce=r)
+    loss_i = fun.mse_loss(pred_images, target_images, reduce=r)
+    loss_s = fun.mse_loss(pred_states, target_states, reduce=r)
+    loss_c = fun.mse_loss(pred_costs.cuda(), target_costs.cuda(), reduce=r)
     return loss_i, loss_s, loss_c
+
 
 dataloader.random.seed(12345)
 
 for i in range(opt.n_batches):
-    torch.cuda.empty_cache()    
-    inputs_, actions_, targets_, _, _ = dataloader.get_batch_fm('test', opt.npred)
+    with torch.no_grad():
+        torch.cuda.empty_cache()
+        inputs, actions, targets, _, _ = dataloader.get_batch_fm('test', opt.npred)
 
-    if i < 10 and opt.save_video:
-        for b in range(opt.batch_size):
-            dirname_movie = '{}/videos/x{:d}/y/'.format(dirname, i*opt.batch_size + b)
-            print('[saving ground truth video: {}]'.format(dirname_movie))
-            utils.save_movie(dirname_movie, targets_[0][b], targets_[1][b], targets_[2][b])
-            
-    for s in range(opt.n_samples):
-        print('[batch {}, sample {}'.format(i, s), end="\r")
-        inputs = utils.make_variables(inputs_)
-        targets = utils.make_variables(targets_)
-        actions = utils.Variable(actions_)
-
-        if opt.zeroact == 1:
-            actions.data.zero_()
-
-        pred_, _= model(inputs, actions, targets, sampling=opt.sampling)
-        '''
-        loss_i_s, loss_s_s, loss_c_s = compute_loss(targets, pred_, r=False)
-        loss_i[i, :, s] += loss_i_s.mean(2).mean(2).mean(2).data.cpu()
-        loss_s[i, :, s] += loss_s_s.mean(2).data.cpu()
-        loss_c[i, :, s] += loss_c_s.mean(2).data.cpu()
-        '''
-#        pred_costs[i, :, s].copy_(pred_[2].data)
-#        true_costs[i].copy_(targets[2].data)
-        pred_states[i, :, s].copy_(pred_[1].data)
-        true_states[i].copy_(targets[1].data)
-
-        if i < 10 and s < 20 and opt.save_video:
+        # save ground truth for the first 10 x batch_size samples
+        if i < 10 and opt.save_video:
             for b in range(opt.batch_size):
-                dirname_movie = '{}/videos/sampled_z/true_actions/x{:d}/z{:d}/'.format(dirname, i*opt.batch_size + b, s)
-                print('[saving video: {}]'.format(dirname_movie), end="\r")
-                utils.save_movie(dirname_movie, pred_[0][b].data, pred_[1][b].data)#, pred_[2][b].data)
+                dirname_movie = f'{dirname}/videos/x{i * opt.batch_size + b:d}/y/'
+                print(f'[saving ground truth video: {dirname_movie}]')
+                utils.save_movie(dirname_movie, targets[0][b], targets[1][b], targets[2][b])
 
-        actions_perm = actions[torch.randperm(opt.batch_size).long().cuda()]
+        for s in range(opt.n_samples):
+            print(f'[batch {i}, sample {s}]', end="\r")
 
-        # also generate videos with different action sequences
-        pred_perm, _= model(inputs, actions_perm, targets, sampling=opt.sampling)
-        if i < 10 and s < 20 and opt.save_video:
-            for b in range(opt.batch_size):
-                dirname_movie = '{}/videos/sampled_z/perm_actions/x{:d}/z{:d}/'.format(dirname, i*opt.batch_size + b, s)
-                print('[saving video: {}]'.format(dirname_movie), end="\r")
-                utils.save_movie(dirname_movie, pred_perm[0][b].data, pred_perm[1][b].data)#, pred_perm[2][b].data)
+            if opt.zeroact == 1:
+                actions.data.zero_()
 
+            pred, _ = model(inputs, actions, targets, sampling=opt.sampling)  # return as many predictions as actions
+            pred_states[i, :, s].copy_(pred[1])
+            true_states[i].copy_(targets[1])
 
-        # also generate videos with true z vectors
-        if s == 0:
-            pred_true_z, _= model(inputs, actions, targets)
-            for b in range(opt.batch_size):
-                dirname_movie = '{}/videos/true_z/true_actions/x{:d}/z{:d}/'.format(dirname, i*opt.batch_size + b, s)
-                print('[saving video: {}]'.format(dirname_movie), end="\r")
-                utils.save_movie(dirname_movie, pred_true_z[0][b].data, pred_true_z[1][b].data)#, pred_true_z[2][b].data)
+            if i < 10 and s < 20 and opt.save_video:
+                for b in range(opt.batch_size):
+                    dirname_movie = f'{dirname}/videos/sampled_z/true_actions/x{i * opt.batch_size + b:d}/z{s:d}/'
+                    print('[saving video: {}]'.format(dirname_movie), end="\r")
+                    utils.save_movie(dirname_movie, pred[0][b], pred[1][b])  # , pred_[2][b])
+                    #                                    ^ images    ^ position and velocity
 
-            pred_true_z_perm, _= model(inputs, actions_perm, targets)
-            for b in range(opt.batch_size):
-                dirname_movie = '{}/videos/true_z/perm_actions/x{:d}/z{:d}/'.format(dirname, i*opt.batch_size + b, s)
-                print('[saving video: {}]'.format(dirname_movie), end="\r")
-                utils.save_movie(dirname_movie, pred_true_z_perm[0][b].data, pred_true_z_perm[1][b].data)#, pred_true_z_perm[2][b].data)
+            # rotate actions across the batch: a_{t} -> a_{t + 1}
+            actions_rot = actions[(torch.arange(opt.batch_size) - 1) % opt.batch_size]
 
+            # also generate videos with different action sequences
+            pred_rot, _ = model(inputs, actions_rot, targets, sampling=opt.sampling)
+            if i < 10 and s < 20 and opt.save_video:
+                for b in range(opt.batch_size):
+                    dirname_movie = f'{dirname}/videos/sampled_z/rot_actions/x{i * opt.batch_size + b:d}/z{s:d}/'
+                    print('[saving video: {}]'.format(dirname_movie), end="\r")
+                    utils.save_movie(dirname_movie, pred_rot[0][b], pred_rot[1][b])  # , pred_perm[2][b])
 
+            # also generate videos with true z vectors
+            if s == 0:
+                pred_true_z, _ = model(inputs, actions, targets)
+                for b in range(opt.batch_size):
+                    dirname_movie = f'{dirname}/videos/true_z/true_actions/x{i * opt.batch_size + b:d}/z{s:d}/'
+                    print('[saving video: {}]'.format(dirname_movie), end="\r")
+                    utils.save_movie(dirname_movie, pred_true_z[0][b], pred_true_z[1][b])  # , pred_true_z[2][b])
 
+                pred_true_z_rot, _ = model(inputs, actions_rot, targets)
+                for b in range(opt.batch_size):
+                    dirname_movie = f'{dirname}/videos/true_z/rot_actions/x{i * opt.batch_size + b:d}/z{s:d}/'
+                    print('[saving video: {}]'.format(dirname_movie), end="\r")
+                    utils.save_movie(dirname_movie, pred_true_z_rot[0][b], pred_true_z_rot[1][b])
+                    # , pred_true_z_perm[2][b])
 
-        del inputs, actions, targets, pred_
+            # del inputs, actions, targets, pred
 
-torch.save({'loss_i': loss_i, 
-            'loss_s': loss_s, 
-            'loss_c': loss_c, 
-            'true_costs': true_costs, 
-            'pred_costs': pred_costs, 
-            'true_states': true_states, 
+torch.save({'loss_i': loss_i,
+            'loss_s': loss_s,
+            'loss_c': loss_c,
+            'true_costs': true_costs,
+            'pred_costs': pred_costs,
+            'true_states': true_states,
             'pred_states': pred_states},
            f'{dirname}/loss.pth')
 
-os.system('tar -cvf {}.tar.gz {}'.format(dirname, dirname))
+os.system(f'tar -cvf {dirname}.tgz {dirname}')
