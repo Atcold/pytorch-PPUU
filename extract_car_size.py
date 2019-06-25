347d77f10cb09f0d7b913076bfbfdabc38575ae3
Yair Schiff <yzs208@nyu.edu>
2019-06-25 10:26:45 -0400

    Enable extract_car_size.py and dataloader.py to handle US101 map dynamically.

--- extract_car_size.py
+++ extract_car_size.py
@@ -1,12 +1,16 @@
+import argparse
+import os
+
 import pandas as pd
 import torch
 
-path = './traffic-data/xy-trajectories/i80/'
-time_slots = (
-    'trajectories-0400-0415',
-    'trajectories-0500-0515',
-    'trajectories-0515-0530',
-)
+parser = argparse.ArgumentParser()
+parser.add_argument('-map', type=str, default='i80', choices={'ai', 'i80', 'us101', 'lanker', 'peach'})
+opt = parser.parse_args()
+
+path = './traffic-data/xy-trajectories/{}/'.format(opt.map)
+trajectories_path = './traffic-data/state-action-cost/data_{}_v0'.format(opt.map)
+time_slots = [d[0].split("/")[-1] for d in os.walk(trajectories_path) if d[0] != trajectories_path]
 
 df = dict()
 for ts in time_slots:
@@ -30,10 +34,9 @@ for ts in time_slots:
         'Spacing',
         'Headway'
     ))
-    
 
 car_sizes = dict()
-for ts in time_slots:    
+for ts in time_slots:
     d = df[ts]
     car = lambda i: d[d['Vehicle ID'] == i]
     car_sizes[ts] = dict()
@@ -43,5 +46,5 @@ for ts in time_slots:
             size = tuple(car(c).loc[car(c).index[0], ['Vehicle Width', 'Vehicle Length']].values)
             car_sizes[ts][c] = size
             print(c)
-        
-torch.save(car_sizes, 'traffic-data/state-action-cost/data_i80_v0/car_sizes.pth')
+
+torch.save(car_sizes, 'traffic-data/state-action-cost/data_{}_v0/car_sizes.pth'.format(opt.map))
