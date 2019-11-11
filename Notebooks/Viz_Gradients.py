b3c7ae0af500367463a3039ff3fa0ee795535a2f
Serkan Karakulak <sk7685@nyu.edu>
2019-11-11 12:21:15 -0500

    added notebook for visualizing gradients wrt actions

---
+++ Notebooks/Viz_Gradients.py
@@ -0,0 +1,58 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.2'
+#       jupytext_version: 1.2.3
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %%
+import torch
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.image import NonUniformImage
+from jupyterthemes import jtplot
+
+# %%
+jtplot.style('oceans16')
+
+# %%
+act_grads = torch.load('../../actions_grads.pkl')
+
+# %%
+data = [np.array(k) for k in act_grads]
+data = np.concatenate(data, axis=0)
+
+# %%
+xedges = [np.quantile(data[:,0], p) for p in np.linspace(0,1,21)]
+
+# %%
+df = pd.DataFrame(data)
+df.columns = ['speed', 'grad_proximity', 'grad_lane']
+df.speed = (df.speed//5 * 5).astype(int)
+
+# %%
+df_new = df.copy()
+
+# %%
+df_new.boxplot(column='grad_proximity', by='speed', figsize=(14,8),)
+
+# %%
+df_new.boxplot(column='grad_lane', by='speed', figsize=(14,8))
+
+# %%
+df_new[['grad_proximity', 'grad_lane']] = np.log10(df_new[['grad_proximity', 'grad_lane']])
+
+# %%
+df_new.boxplot(column='grad_proximity', by='speed', figsize=(14,8))
+
+# %%
+df_new.boxplot(column='grad_lane', by='speed', figsize=(14,8))
