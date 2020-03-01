53c751d6095e8c05151c8ab9e037a54851b5a22e
Yair Schiff <yairschiff@gmail.com>
2020-03-01 12:00:38 -0500

    Enable eval of look aside eval

--- scripts/run_eval_mpur_tl.sh
+++ scripts/run_eval_mpur_tl.sh
@@ -1,28 +1,33 @@
 # Allows named arguments
 set -k
 
-#for policy in ${target_lane_policies[*]}; do
-#policy_dir="/misc/vlgscratch4/LecunGroup/nvidia-collab/yairschiff/pytorch-PPUU/models_learned_cost/policy_networks"
-#for tl in 0.9 0.8 0.7; do
-#    for policy in $policy_dir/*lambdatl=${tl}*.model; do
-#        echo "Working on policy: ${policy:103}..."
-#        sbatch \
-#        --output ../logs/target_lane_learned_cost/planning_results/${policy:103}.out \
-#         --error ../logs/target_lane_learned_cost/planning_results/${policy:103}.err \
-#        submit_eval_mpur_tl.slurm \
-#            model_dir=models_learned_cost \
-#            policy=${policy:103}
-#    done
-#done
+
+# Target lane policy
+policy_dir="/misc/vlgscratch4/LecunGroup/nvidia-collab/yairschiff/pytorch-PPUU/models_learned_cost/policy_networks"
+save_dir="planning_results_look"
+for tl in 1.0; do
+  for ll in 0.0; do
+    for policy in $policy_dir/*lambdal=${ll}*lambdatl=${tl}*.model; do
+      echo "Working on policy: ${policy:103}..."
+      sbatch \
+        --output ../logs/target_lane_learned_cost/planning_results_look/${policy:103}.out \
+        --error ../logs/target_lane_learned_cost/planning_results_look/${policy:103}.err \
+        submit_eval_mpur_tl.slurm \
+          model_dir="models_learned_cost" \
+          policy=${policy:103} \
+          save_dir=${save_dir}
+    done
+  done
+done
 
 # Baseline
-policy_dir="/misc/vlgscratch4/LecunGroup/nvidia-collab/models_v13/policy_networks"
-for policy in $policy_dir/MPUR-policy-deterministic-model=vae-zdropout=0.5-nfeature=256-bsize=6-npred=30-ureg=0.05-lambdal=0.2-lambdaa=0.0-gamma=0.99-lrtz=0.0-updatez=0-inferz=0-learnedcost=False-seed=*-novaluestep*.model; do
-      echo "Working on policy: ${policy:70}..."
-      sbatch \
-      --output ../logs/target_lane_learned_cost/baseline/planning_results/${policy:70}.out \
-      --error ../logs/target_lane_learned_cost/baseline/planning_results/${policy:70}.err \
-      submit_eval_mpur_tl.slurm \
-      model_dir="/misc/vlgscratch4/LecunGroup/nvidia-collab/models_v13/" \
-      policy=${policy:70}
-done
+#policy_dir="/misc/vlgscratch4/LecunGroup/nvidia-collab/models_v13/policy_networks"
+#for policy in $policy_dir/MPUR-policy-deterministic-model=vae-zdropout=0.5-nfeature=256-bsize=6-npred=30-ureg=0.05-lambdal=0.2-lambdaa=0.0-gamma=0.99-lrtz=0.0-updatez=0-inferz=0-learnedcost=False-seed=*-novaluestep*.model; do
+#      echo "Working on policy: ${policy:70}..."
+#      sbatch \
+#      --output ../logs/target_lane_learned_cost/baseline/planning_results/${policy:70}.out \
+#      --error ../logs/target_lane_learned_cost/baseline/planning_results/${policy:70}.err \
+#      submit_eval_mpur_tl.slurm \
+#      model_dir="/misc/vlgscratch4/LecunGroup/nvidia-collab/models_v13/" \
+#      policy=${policy:70}
+#done
