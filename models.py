0b90d19079f8af5785e45a399162d0e99182b397
xjh <414581775@qq.com>
2020-07-14 00:31:59 -0400

    fix bugs

--- models.py
+++ models.py
@@ -20,7 +20,7 @@ class encoder(nn.Module):
         self.n_channels = n_channels
         # colored lane needs an additional channel
         if opt.use_colored_lane:
-            self.n_channels = 4
+            self.n_channels = self.n_channels + 1
         # frame encoder
         if opt.layers == 3:
             assert(opt.nfeature % 4 == 0)
@@ -879,11 +879,7 @@ class DeterministicPolicy(nn.Module):
     def __init__(self, opt, context_dim=0, output_dim=None, n_channels=4):
         super().__init__()
         self.opt = opt
-        if opt.use_colored_lane:
-            self.n_channels = 5
-        else:
-            self.n_channels = 4
-        # self.n_channels = n_channels
+        self.n_channels = n_channels
         self.encoder = encoder(opt, a_size=0, n_inputs=opt.ncond, n_channels=self.n_channels)
         self.n_outputs = opt.n_actions if output_dim is None else output_dim
         self.hsize = opt.nfeature * self.opt.h_height * self.opt.h_width
