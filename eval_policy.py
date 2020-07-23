4682474bf516a89cdb782989e1200ef6e6721a0f
xjh <414581775@qq.com>
2020-07-23 16:22:17 -0400

    try remove parallel

--- eval_policy.py
+++ eval_policy.py
@@ -497,8 +497,8 @@ def main():
 
     n_test = len(splits['test_indx'])
 
-    set_start_method('spawn')
-    pool = Pool(opt.num_processes)
+    #set_start_method('spawn')
+    #pool = Pool(opt.num_processes)
 
     async_results = []
 
@@ -513,8 +513,8 @@ def main():
                         timeslot]][car_id]
                 )[None, :]
         async_results.append(
-            pool.apply_async(
-                process_one_episode, (
+            #pool.apply_async(
+                process_one_episode(
                     opt,
                     env,
                     car_path,
@@ -525,11 +525,12 @@ def main():
                     j,
                     car_sizes
                 )
-            )
+            #)
         )
 
     for j in range(n_test):
-        simulation_result = async_results[j].get()
+        #simulation_result = async_results[j].get()
+        simulation_result = async_results[j]
 
         time_travelled.append(simulation_result.time_travelled)
         distance_travelled.append(simulation_result.distance_travelled)
@@ -568,8 +569,8 @@ def main():
             writer.add_scalar('ByEpisode/Distance',
                               simulation_result.distance_travelled, j)
 
-    pool.close()
-    pool.join()
+    #pool.close()
+    #pool.join()
 
     diff_time = time.time() - time_started
     print('avg time travelled per second is', total_images / diff_time)
