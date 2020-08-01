0c413edf64329aa4da2ffff25fac340e658c8275
xjh <414581775@qq.com>
2020-08-01 16:54:33 -0400

    Single pool

--- eval_policy.py
+++ eval_policy.py
@@ -518,8 +518,8 @@ def main():
 
     n_test = len(splits['test_indx'])
 
-    set_start_method('spawn')
-    pool = Pool(opt.num_processes)
+    # set_start_method('spawn')
+    # pool = Pool(opt.num_processes)
 
     async_results = []
 
@@ -534,24 +534,24 @@ def main():
                         timeslot]][car_id]
                 )[None, :]
         async_results.append(
-            pool.apply_async(
-                process_one_episode, (
-                    opt,
-                    env,
-                    car_path,
-                    forward_model,
-                    policy_network_il,
-                    data_stats,
-                    plan_file,
-                    j,
-                    car_sizes,
-                )
+            # pool.apply_async(
+            process_one_episode(
+                opt,
+                env,
+                car_path,
+                forward_model,
+                policy_network_il,
+                data_stats,
+                plan_file,
+                j,
+                car_sizes
             )
+            # )
         )
 
     for j in range(n_test):
-        simulation_result = async_results[j].get()
-
+        # simulation_result = async_results[j].get()
+        simulation_result = async_results[j]
         time_travelled.append(simulation_result.time_travelled)
         distance_travelled.append(simulation_result.distance_travelled)
         road_completed.append(simulation_result.road_completed)
@@ -589,8 +589,8 @@ def main():
             writer.add_scalar('ByEpisode/Distance',
                               simulation_result.distance_travelled, j)
 
-    pool.close()
-    pool.join()
+    #pool.close()
+    #pool.join()
 
     diff_time = time.time() - time_started
     print('avg time travelled per second is', total_images / diff_time)
